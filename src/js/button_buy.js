5959c89eb64f281255597dad23a4003083616de9
Juan Munguia~ <carlosmunguia23145@gmail.com>
2023-11-02 09:02:09 +0100

    pull changes for dev

---
+++ src/js/button_buy.js
@@ -0,0 +1,9 @@
+const buton_container = document.querySelector('#buy_section')
+
+let allProducts = []
+
+buton_container.addEventListener('click', e =>{
+    if(e.target.classList.contain('.button_buy')){
+        
+    }
+})
